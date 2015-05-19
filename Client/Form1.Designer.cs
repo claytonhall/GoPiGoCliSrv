4c471accf801724c9c8d0472cee8b2cebc5a468f
claytonhall <clayton.hall@gmail.com>
2015-05-19 16:49:04 -0500

    Adding to repository

---
+++ Client/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace WallE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIPAddress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnFwd = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.tbServo = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCommand = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.tbServo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtIPAddress
+            // 
+            this.txtIPAddress.Location = new System.Drawing.Point(74, 12);
+            this.txtIPAddress.Name = "txtIPAddress";
+            this.txtIPAddress.Size = new System.Drawing.Size(99, 20);
+            this.txtIPAddress.TabIndex = 0;
+            this.txtIPAddress.Text = "192.168.2.140";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP Address";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(263, 10);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(59, 23);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnFwd
+            // 
+            this.btnFwd.Location = new System.Drawing.Point(155, 103);
+            this.btnFwd.Name = "btnFwd";
+            this.btnFwd.Size = new System.Drawing.Size(75, 23);
+            this.btnFwd.TabIndex = 3;
+            this.btnFwd.Text = "fwd()";
+            this.btnFwd.UseVisualStyleBackColor = true;
+            this.btnFwd.KeyDown += new System.Windows.Forms.KeyEventHandler(this.btnDir_KeyDown);
+            this.btnFwd.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnDir_MouseDown);
+            this.btnFwd.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btnDir_MouseUp);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(155, 132);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "stop()";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox1.Location = new System.Drawing.Point(0, 301);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(404, 210);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(182, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Port";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(211, 12);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(46, 20);
+            this.txtPort.TabIndex = 6;
+            this.txtPort.Text = "5005";
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(328, 9);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(69, 23);
+            this.btnDisconnect.TabIndex = 8;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Location = new System.Drawing.Point(74, 132);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(75, 23);
+            this.btnLeft.TabIndex = 9;
+            this.btnLeft.Text = "left()";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnDir_MouseDown);
+            this.btnLeft.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btnDir_MouseUp);
+            // 
+            // btnRight
+            // 
+            this.btnRight.Location = new System.Drawing.Point(236, 132);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(75, 23);
+            this.btnRight.TabIndex = 10;
+            this.btnRight.Text = "right()";
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnDir_MouseDown);
+            this.btnRight.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btnDir_MouseUp);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(155, 161);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.TabIndex = 11;
+            this.btnBack.Text = "bwd()";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnDir_MouseDown);
+            this.btnBack.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btnDir_MouseUp);
+            // 
+            // tbServo
+            // 
+            this.tbServo.Location = new System.Drawing.Point(74, 197);
+            this.tbServo.Maximum = 180;
+            this.tbServo.Name = "tbServo";
+            this.tbServo.Size = new System.Drawing.Size(237, 45);
+            this.tbServo.TabIndex = 12;
+            this.tbServo.Value = 90;
+            this.tbServo.ValueChanged += new System.EventHandler(this.tbServo_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(74, 246);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(237, 33);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Get Ultra Sonic Reading";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnUS_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Command";
+            // 
+            // txtCommand
+            // 
+            this.txtCommand.Location = new System.Drawing.Point(77, 40);
+            this.txtCommand.Name = "txtCommand";
+            this.txtCommand.Size = new System.Drawing.Size(99, 20);
+            this.txtCommand.TabIndex = 14;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(185, 38);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 16;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 511);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCommand);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbServo);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnLeft);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnFwd);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIPAddress);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tbServo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIPAddress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnFwd;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.TrackBar tbServo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCommand;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ Client/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WallE
+{
+    public partial class Form1 : Form
+    {
+        bool _clicked = false;
+        WallEsBrain _wallEsBrain = new WallEsBrain();
+      
+
+        public Form1()
+        {
+            InitializeComponent();
+            _wallEsBrain.OnLog += Log;
+
+            tbServo.MouseDown += (s, e) =>
+            {
+                _clicked = true;
+            };
+
+            tbServo.MouseUp += (s, e) =>
+            {
+                if (!_clicked)
+                    return;
+
+                _clicked = false;
+                
+            };
+
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            _wallEsBrain.Connect(txtIPAddress.Text, txtPort.Text);
+        }
+
+        private void btnFwd_Click(object sender, EventArgs e)
+        {
+            _wallEsBrain.SendCommand("fwd()");
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            _wallEsBrain.SendCommand("stop()");
+        }
+
+        void Log(string s)
+        {
+            textBox1.AppendText(s + "\r\n");
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            _wallEsBrain.Disconnect();
+        }
+
+        private void btnDir_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void btnDir_MouseDown(object sender, MouseEventArgs e)
+        {
+            _wallEsBrain.SendCommand(((Button)sender).Text);
+        }
+
+        private void btnDir_MouseUp(object sender, MouseEventArgs e)
+        {
+            _wallEsBrain.SendCommand("stop()");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUS_Click(object sender, EventArgs e)
+        {
+            _wallEsBrain.SendCommand("us_dist(15)");
+        }
+
+        private void tbServo_ValueChanged(object sender, EventArgs e)
+        {
+            if (!_clicked)
+            {
+                _wallEsBrain.SendCommand(String.Format("servo({0})", tbServo.Value));
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            _wallEsBrain.SendCommand(txtCommand.Text);
+        }
+    }
+
+    public delegate void LoggerDelegate(string s);
+
+    public class WallEsBrain
+    {
+        public string IP { get; set; }
+        public string Port { get; set; }
+        public Socket Socket { get; set; }
+
+        public LoggerDelegate OnLog;
+        
+
+        public bool Connect(string ip, string port)
+        {
+            IP = ip;
+            Port = port;
+            return Connect();
+        }
+
+        public bool Connect()
+        {
+            bool retVal = false;
+
+            try
+            {
+                this.OnLog(String.Format("Connect to {0}:{1}", IP, Port));
+
+                this.Socket = new Socket(AddressFamily.InterNetwork,
+                    SocketType.Stream, ProtocolType.Tcp);
+
+                IPEndPoint ipe = new IPEndPoint(IPAddress.Parse(IP), int.Parse(Port));
+                this.Socket.Connect(ipe);
+
+                if (this.Socket.Connected)
+                {
+                    retVal = true;
+                }
+            }
+            catch(Exception ex)
+            {
+                OnLog(ex.ToString());
+            }
+            return retVal;
+        }
+
+        public void Disconnect()
+        {
+            if (this.Socket.Connected)
+            {
+                this.Socket.Disconnect(true);
+            }
+        }
+
+        public string SendCommand(string s)
+        {
+            this.OnLog(String.Format("Sending: {0}", s));
+
+            var base64 = Convert.ToBase64String(ASCIIEncoding.ASCII.GetBytes(s));
+            var i = this.Socket.Send(ASCIIEncoding.ASCII.GetBytes(base64));
+
+            byte[] buffer = new byte[1024];
+            int result = this.Socket.Receive(buffer);
+            
+            this.OnLog(String.Format("Received: {0}", ASCIIEncoding.ASCII.GetString(buffer).Substring(0,result)));
+            
+            return i.ToString();
+        }
+
+    }
+}
